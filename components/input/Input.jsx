0f9b8141a3abb2101ad5ce210e19d59e992db538
joao victor <joaovictor@agencia-flow.com>
2021-09-09 12:44:56 -0300

    Init: Project

---
+++ components/input/Input.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+
+// import { Container } from './styles';
+
+function input() {
+  useEffect(() => {
+    // Função qualquer
+  }, []);
+  return <div />;
+}
+
+export default input;
